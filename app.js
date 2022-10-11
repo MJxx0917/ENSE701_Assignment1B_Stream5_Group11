68f93efac7cd6abc4981e7cec85a558d5c603a94
Wenxi Qiao <alexqiao860315@hotmail.com>
2022-10-11 21:45:24 +1300

    Alex change

--- app.js
+++ app.js
@@ -36,7 +36,6 @@ if(process.env.NODE_ENV === "production")
         res.send("Api running");
     });
 }
-
 const port = process.env.PORT || 8082;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
